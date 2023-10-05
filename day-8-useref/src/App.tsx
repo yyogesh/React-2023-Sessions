2fcea83a426480a57892b435edf0316f44d7c59a
yyogesh <yogeshggn@hotmail.com>
2023-10-05 16:19:19 +0530

    useRef

--- day-8-useref/src/App.tsx
+++ day-8-useref/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
+import StopWatch from './components/StopWatch';
 
 function App() {
   const [count, setCount] = useState<number>(0);
@@ -32,6 +33,7 @@ function App() {
     countRef.current += 1;
     console.log('countRef value', countRef.current);
     inputRef.current.focus();
+    console.log('inputRef value', inputRef.current);
   }
 
   console.log('reload the component count value', count, countRef.current);
@@ -40,6 +42,8 @@ function App() {
       <input type='text' ref={inputRef}/>
       <button onClick={handleClick}>{'Count ' + count}</button>
       <button onClick={handleRefClick}>{'RefCount ' + countRef.current}</button>
+      <hr/>
+      <StopWatch/>
     </div>
   );
 }
--- day-8-useref/src/components/StopWatch.tsx
+++ day-8-useref/src/components/StopWatch.tsx
@@ -1,28 +1,35 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const StopWatch = () => {
     const [count, setCount] = useState(0);
-    // const timerIdRef = useRef<Timer>();
+     const intervalIdRef = useRef<any>();
 
-    
-    // const startHandler = () => { 
-    //     if (timerIdRef.current) { return; }
+    const startHandler = () => { 
+        if (intervalIdRef.current) { 
+            return; 
+        }
 
-    //     timerIdRef.current = setInterval(() => setCount(prev => {
-    //         return prev + 1;
-    //     }), 1000)
-    // }
-    // const stopHandler = () => { }
+        intervalIdRef.current = setInterval(() => setCount(prev => { // keep calling after 1 sec
+            return prev + 1;
+        }), 1000)
+    }
+    const stopHandler = () => { 
+        clearInterval(intervalIdRef.current);
+        intervalIdRef.current = 0;
+    }
 
 
-
+    useEffect(() => {
+        // on component unmount clear internal
+        return () => clearInterval(intervalIdRef.current);
+    }, [])
 
     return (
         <div>
             <div>Timer: {count}s</div>
             <div>
-                {/* <button onClick={startHandler}>Start</button>
-                <button onClick={stopHandler}>Stop</button> */}
+                <button onClick={startHandler}>Start</button>
+                <button onClick={stopHandler}>Stop</button>
             </div>
         </div>
     )
